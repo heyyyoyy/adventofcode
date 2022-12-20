5ebec36644a52f315492079d82ba6604e9dbeff6
Sergey <heyyyoyy@gmail.com>
2022-12-20 20:03:05 +0300

    fix zero shift

--- src/aoc_2022/day_20/part_1.rs
+++ src/aoc_2022/day_20/part_1.rs
@@ -13,8 +13,12 @@ fn grove_positioning_system(input_str: &str) -> i32 {
         let index = mixed.iter().position(|&num| num.0 == *idx).unwrap();
         let rem_value = mixed.remove(index);
         let shift = index as i32 + rem_value.1;
-        let new_index = shift.rem_euclid(mixed.len() as i32) as usize;
-        mixed.insert(new_index, rem_value);
+        if shift == 0 {
+            mixed.push(rem_value);
+        } else {
+            let new_index = shift.rem_euclid(mixed.len() as i32) as usize;
+            mixed.insert(new_index, rem_value);
+        }
     }
 
     let zero_index = mixed.iter().position(|&num| num.1 == 0).unwrap();
